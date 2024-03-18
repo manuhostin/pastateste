78ff91a5756ad495e6aaf4b8d5cf3e370bb2acf4
manuhostin <hostimmanu@gmail.com>
2024-03-18 16:56:43 -0300

    Alteracoes iniciais

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<script setup></script>
+
+<template>
+  <h1 class="titulo">Formulario:</h1>
+  <p>Nome:</p>
+  <input type="text" v-model="nome" minlength="10" placeholder="Digite seu nome" />
+  <input type="email" v-model="email" minlength="10" placeholder="Digite seu email" />
+  <input type="password" v-model="senha" placeholder="Digite sua senha"/>
+
+
+</template>
+
+<style scoped>
+
+@import url('https://fonts.googleapis.com/css2?family=Mallanna&display=swap');
+.titulo {
+  text-align: center;
+  font-family: 'Mallanna', sans-serif;
+}
+p {
+  font-family: 'Mallanna', sans-serif;
+}
+</style>
